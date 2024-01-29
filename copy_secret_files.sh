89ebddc1c2eafd31b803cd5b95b7434e0259135e
schef <zvanstefan@gmail.com>
2024-01-29 11:59:09 +0100

    added copy script

---
+++ copy_secret_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ../ssc_credentials/classroombookings_docker/default.env ./
+cp ../ssc_credentials/classroombookings_scripts/asctt2012.xml ./crbs-scripts/
+cp ../ssc_credentials/classroombookings_scripts/credentials.py ./crbs-scripts/
